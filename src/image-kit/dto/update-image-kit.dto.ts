0fcf0cb1152fc1a0ea834fac97284a4fb3b902e0
mohansharma916 <mohansharma916@gmail.com>
2023-11-19 10:57:16 +0530

    error corrected

--- src/image-kit/dto/update-image-kit.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateImageKitDto } from './create-image-kit.dto';
-
-export class UpdateImageKitDto extends PartialType(CreateImageKitDto) {}
--- src/image-kit/image-kit.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ImageKitService } from './image-kit.service';
-import { ImageKitController } from './image-kit.controller';
-
-@Module({
-  controllers: [ImageKitController],
-  providers: [ImageKitService]
-})
-export class ImageKitModule {}
