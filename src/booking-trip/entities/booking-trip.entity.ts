f5a7f9d37f149b29988df1d8b9ed8c5eb4483db4
mohansharma916 <mohansharma916@gmail.com>
2023-11-19 16:48:01 +0530

    changed schema

--- src/booking-trip/entities/booking-trip.entity.ts
+++ src/booking-trip/entities/booking-trip.entity.ts
@@ -2,6 +2,12 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class BookingTrip {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Trip Date', nullable: true })
+  tripDate?: string;
+
+  @Field(() => String, { description: 'User Id', nullable: true })
+  userId?: string;
+
+  @Field(() => Int, { description: 'Trip Id', nullable: true })
+  tripId: number;
 }
